0ede794b6caf85d202f0261149d1f9a3aeb6dac4
mgmraz <mary.g.mraz@gmail.com>
2021-10-15 14:52:55 -0400

    edit

--- src/teef.java
+++ src/teef.java
@@ -7,10 +7,10 @@ public class teef {
     public static void main(String[] args) {
         int numFam;
         int index;
+        String menuResponse;
         String[] names = new String[5];
-        String[] uppers = new String[10];
+        String[] uppers = new String [10];
         String[] lowers = new String[10];
-        String[] acceptedLetters = {"B", "C", "M", "b", "c", "m"};  // may or may not use if i can figure it out
 
     //--- do while loop for number of family members (max of 5)
         do {
@@ -22,24 +22,45 @@ public class teef {
         for (index = 0; index < numFam; index++) {
             System.out.println("Please enter the name for family member" + (index + 1));
             names[index] = keyboard.next();
-    //--- getting uppers and lowers with restraints with a do while loop: max of 10, only "B" "C" "M"
+    //--- getting uppers and lowers with restraints with a do while loop: max of 10, only B C M or b c m
             do {
             System.out.println("Please enter the uppers for " + names[index]);
             uppers[index] = keyboard.next();
-            } while (uppers[index].length() > 10 || !(uppers[index].matches("[BCMbcm]")));
+            } while (uppers[index].length() > 10 || !(uppers[index].matches("^[BCMbcm]*")));
             do {
                 System.out.println("Please enter the lowers for " + names[index]);
                 lowers[index] = keyboard.next();
-            } while (lowers[index].length() > 10 || !(lowers[index].matches("[BCMbcm]")));
+            } while (lowers[index].length() > 10 || !(lowers[index].matches("^[BCMbcm]*")));
 
-                    //!(lowers[index].contains(acceptedAnswers)));
+        }
 
+    do {
+        System.out.println("(P)rint, (E)xtract, (R)oot, e(X)it");
+        menuResponse = keyboard.next().toUpperCase();
 
+        if (menuResponse.equals("P")) {
+            System.out.println("test");
+                System.out.println("test");
 
+        } else if (menuResponse.equals("E")) {
+                System.out.println("test");
+
+        } else if (menuResponse.equals("R")) {
+                System.out.println("test");
+        }
+    } while (!menuResponse.equals("X"));
+            System.out.println("Exiting the Floridian Tooth Records :)");
 
 
         }
 
-    }
+
+
+
+
+
+
+//--------------------------------------------------------------------------------------------------------------
 }
+
 
